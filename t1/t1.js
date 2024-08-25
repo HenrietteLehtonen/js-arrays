a51df8e061e421b57b934672000beceaaf4e63ff
Henriette Lehtonen <Henriette.Lehtonen@student.lab.fi>
2024-08-25 15:10:37 +0300

    teht 1 valmis.

---
+++ t1/t1.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// i.
+let fruits = [];
+
+// ii.
+fruits = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
+console.log('Fruits:', fruits);
+
+// iii.
+console.log('Lenght of fruits: ', fruits.length);
+
+/// iv.
+console.log('Element at Index 2', fruits[2]);
+
+// v.
+let lastEl = fruits[fruits.length - 1];
+console.log('Last Element of fruits:', lastEl);
+
+// vi.
+let vegetables = [];
+
+// vii.
+let veg1 = prompt('Anna kasvis 1:');
+vegetables.push(veg1);
+let veg2 = prompt('Anna kasvis 2:');
+vegetables.push(veg2);
+let veg3 = prompt('Anna kasvis 3:');
+vegetables.push(veg3);
+
+console.log('Vegetables: ', vegetables);
+console.log('Lenght of vegetables: ', vegetables.length);
